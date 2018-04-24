b0c4589d013c243e30816ac958664f373e87a7f3
CristianPeralta <cbperaltasegura@gmail.com>
2018-04-24 08:20:37 -0500

    add incident model

---
+++ models/Incident.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+let Schema = mongoose.Schema;
+let incidentSchema = new Schema({
+    name: {type: String, required: [true, 'Name is required']},
+    status: {type: String, default: false},
+    client: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    registeredBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    createdAt:{ type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('Incident',incidentSchema);
