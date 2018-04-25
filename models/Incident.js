f3f138309ad76f6c2413d0a5dddb13a224e60333
CristianPeralta <cbperaltasegura@gmail.com>
2018-04-25 07:05:10 -0500

    add properties to Incident Schema

--- models/Incident.js
+++ models/Incident.js
@@ -2,11 +2,15 @@ import mongoose from 'mongoose'
 
 let Schema = mongoose.Schema;
 let incidentSchema = new Schema({
-    name: {type: String, required: [true, 'Name is required']},
     status: {type: String, default: false},
-    client: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    registeredBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    status: String,
+    priority: String,
+    category: String,
     photo: String,
+    client: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    technician: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    dependence: {type: mongoose.Schema.Types.ObjectId, ref: 'Dependence'},
+    registeredBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     createdAt:{ type: Date, default: Date.now}
 });
 
